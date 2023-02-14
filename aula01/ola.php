d62687e029c6ff7429db04ccc4d966581a19f5ad
Guilhermecintrasss <guilhermecintrass@gmail.com>
2023-02-14 11:00:48 -0300

    Meu primeiro programa PHP

---
+++ aula01/ola.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "
+<ol>
+<li><h1><strong>Ola Mundo</strong></h1></li>
+<li><h2>Ola mundinho</h2></li>
+<li><h3>Ola mundim-inho</h3></li>
+<li><h4>Ola mini mundim-inho</h4></li>
+<li><h5>Ola mundinho minúsculo</h5></li>
+<li><h6>Ola mundinho microscopico</h6></li>
+</ol>
+";
+
+?>
---
+++ aula01/ola2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Aqui é o codigo HTML -->
+    <h1> Exemplo olá mundo </h1>;
+
+    <?php 
+     // Aqui é o código PHP
+     // qualquer coisa fora desse do "<?php" é html
+    echo "
+    
+    <h2>Olá IFSP</h2>
+    
+    ";
+    ?>
+</body>
+</html>
