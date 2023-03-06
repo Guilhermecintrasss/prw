f0c6f76a9c4feb64b424a9742271f03eac08da2c
Guilhermecintrasss <guilhermecintrass@gmail.com>
2023-03-06 09:33:05 -0300

    oi

--- aula02/exercicio15.php
+++ aula02/exercicio15.php
@@ -4,9 +4,6 @@ $linha2 = (readline());
 $x = explode(" ",$linha1);
 $y = explode(" ",$linha2);
 
-$total = (($y[1] - $x[0]) * ($y[1] - $x[0]) + 
-($x[1] - $y[0]) * ($x[1] - $y[0]));
-
 $totalf = number_format(sqrt(($y[1] - $x[1]) * ($y[1] - $x[1]) + 
 ($x[0] - $y[0]) * ($x[0] - $y[0])),4,".","");
 echo "$totalf\n";
