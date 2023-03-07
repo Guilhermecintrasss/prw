67736f7f067af22fd677094c68330a3de9c8b050
Guilhermecintrasss <guilhermecintrass@gmail.com>
2023-03-07 09:41:28 -0300

    oi

---
+++ aula03/exercicio1.php
@@ -0,0 +1,11 @@
+<?php 
+
+$linha1 = readline();
+$v = explode(" ",$linha1);
+
+if (($v[1] > $v[2]) & ($v[0] > $v[3]) & ($v[2] + $v[3] > $v[0] + $v[1])
+& ($v[2] > 0) & ($v[3] > 0) &  (($v[0]%2) == 0)){
+        echo "Valores aceitos\n";
+}
+
+?>
