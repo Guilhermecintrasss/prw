7202fb49c4924fa5b109117a7f81b02de314b6bf
Guilhermecintrasss <guilhermecintrass@gmail.com>
2023-04-04 12:20:02 -0300

    v

--- simulado/exercicio3.php
+++ simulado/exercicio3.php
@@ -2,9 +2,12 @@
 
 $x = readline();
 $i = 0;
-$pts = 0;
-$ptb = 0;
-$pta = 0;
+$tot_saque = 0;
+$tot_bloqe = 0;
+$tot_ataqu = 0;
+$ace_saque = 0;
+$ace_bloqe = 0;
+$ace_ataqu = 0;
 while($i < $x) {
     $nome = readline();
     $linha1 = readline();
@@ -12,21 +15,23 @@ while($i < $x) {
     $linha2 = readline();
     $v2 = explode(" ",$linha2);
     
-    $ps = ($v2[0]/$v1[0]);
-    $pb = ($v2[1]/$v1[1]);
-    $pa = ($v2[2]/$v1[2]);
-    $pts = $ps + $pts;
-    $ptb = $pb + $ptb;
-    $pta = $pa + $pta;
+    $tot_saque += $v1[0];
+    $tot_bloqe += $v1[1];
+    $tot_ataqu += $v1[2];
+
+    $ace_saque += $v2[0];
+    $ace_bloqe += $v2[1];
+    $ace_ataqu += $v2[2];
+
     $i++;
 }
 
-$pts = number_format(($pts/$x)*100,2,".","");
-$ptb = number_format(($ptb/$x)*100,2,".","");
-$pta = number_format(($pta/$x)*100,2,".","");
+$pts = number_format($ace_saque/$tot_saque*100,2,".","");
+$ptb = number_format($ace_bloqe/$tot_bloqe*100,2,".","");
+$pta = number_format($ace_ataqu/$tot_ataqu*100,2,".","");
 
-echo "Pontos de Saque: $pts\n";
-echo "Pontos de Bloqueio: $ptb\n";
-echo "Pontos de Ataque: $pta\n";
+echo "Pontos de Saque: $pts %.\n";
+echo "Pontos de Bloqueio: $ptb %.\n";
+echo "Pontos de Ataque: $pta %.\n";
 
 ?>
