0c96fe9e36e1ea0d63e2e07cda6a0729a241c166
Guilhermecintrasss <guilhermecintrass@gmail.com>
2023-04-03 21:38:00 -0300

    c

---
+++ lista06/exercicio5.php
@@ -0,0 +1,29 @@
+<?php 
+
+$i=0;
+$c=0;
+$f=0;
+$x= intval(readline());
+while($i < $x) {
+
+    $num[$i] = intval(fgets(STDIN));
+    while ($num[$i] != ($c-1)) {
+
+        if ($c == 0){
+           $fibo[$c] = 0; 
+        } else {
+            if ($c == 1) {
+                $fibo[$c] = 1;
+            } else{
+                $fibo[$c] = $fibo[$c-1] + $fibo[$c-2];
+            }
+        }
+        $c++;
+    }
+    
+    echo "Fib(".$num[$i].") = ".$fibo[$c];
+
+    $i++;
+}
+
+?>
