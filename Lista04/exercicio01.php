0997281539e37966168cfdf305012e50620f0e3f
Guilhermecintrasss <guilhermecintrass@gmail.com>
2023-04-07 21:08:08 -0300

    a

--- Lista04/exercicio01.php
+++ Lista04/exercicio01.php
@@ -1,22 +1,22 @@
 <?php 
-
 $linha = readline();
 $num = explode(" ",$linha);
 $X =  $num[0];
 $Y = $num[1];
 $n = 0;
 if ((1<$X) and ($X<20)){
-if (($X < $Y) and ($Y<100000)) {
-
-
 
 while ($n < $Y) {
   $n = $n+1;
   
-  if ($n == 99){ 
+  if ($n == $Y){ 
   echo "$n\n";
 } else {
+  if (($n%$X) === 0) {
+    echo "$n";
+  }else{ 
     echo "$n ";
+  }
 }
   if (($n%$X) === 0)
   {
@@ -24,7 +24,4 @@ while ($n < $Y) {
   }
 }
 }
-}
-
-
 ?>
---
+++ lista03/exercicio5.php
@@ -0,0 +1,10 @@
+<?php 
+$a=0;
+for($i=0;$i<5;$i++){
+    $v = readline();
+    if($v%2==0){
+        $a +=1;
+    }
+}
+echo "$a valores pares\n";
+?>
